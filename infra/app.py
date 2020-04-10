67861931e2fcb55a81b6426714703dcf524b82fa
joel <gitlab@joelashman.net>
2020-04-10 10:43:22 -0700

    clean, fixing env/ssm vars

--- infra/app.py
+++ infra/app.py
@@ -1,11 +1,9 @@
-import os
 from aws_cdk import (
     core,
     aws_s3 as s3,
     aws_codebuild as build,
     aws_codepipeline as pipeline,
     aws_codepipeline_actions as actions,
-    aws_cloudformation as cfn,
 )
 
 
@@ -15,39 +13,32 @@ class EpicollectMigratorLambdaPipelineStack(core.Stack):
 
         artifact_bucket = s3.Bucket(
             self, 'BuildArtifactsBucket',
-            removal_policy=core.RemovalPolicy.RETAIN,
-            encryption=s3.BucketEncryption.KMS_MANAGED,
             versioned=True,
         )
 
         serverless_pipeline = pipeline.Pipeline(
-            self, 'ServerlessPipeline',
+            self, 'epicollect-migrator-pipeline',
             artifact_bucket=artifact_bucket,
-            pipeline_name='serverless-pipeline',
+            pipeline_name='epicollect-migrator-pipeline',
         )
 
         source_output = pipeline.Artifact()
         build_output = pipeline.Artifact()
 
-        secret_id = self.node.try_get_context('github.token.secretmanager.secret.id')
+        secret_id = self.node.try_get_context('github.token.secretsmanager.secret.id')
 
-        github_token = core.SecretValue.secrets_manager(
-            secret_id=secret_id)
+        github_token = core.SecretValue.secrets_manager(secret_id=secret_id)
 
         build_project = build.PipelineProject(
             self, 'BuildProject',
-            project_name='serveless-pipeline',
-            description='Build project for the serverless-pipeline',
-            environment=build.LinuxBuildImage.STANDARD_2_0,
-            environment_variables={
-                'BUILD_ARTIFACT_BUCKET': build.BuildEnvironmentVariable(value=artifact_bucket.bucket_name),
-            },
-            build_spec=build.BuildSpec.from_source_filename('buildspec.yml'),
+            project_name='epicollect-migrator-build',
+            description='Build project for epicollect-migrator',
+            build_spec=build.BuildSpec.from_source_filename('infra/buildspec.yml'),
         )
 
-        serverless_pipeline.add_stage(stage_name='Source', actions=[
+        serverless_pipeline.add_stage(stage_name='CheckoutRepoSource', actions=[
             actions.GitHubSourceAction(
-                action_name='SourceCodeRepo',
+                action_name='CheckoutRepoSource',
                 owner=self.node.try_get_context('github.owner'),
                 oauth_token=github_token,
                 repo=self.node.try_get_context('github.repo'),
@@ -58,7 +49,7 @@ class EpicollectMigratorLambdaPipelineStack(core.Stack):
 
         serverless_pipeline.add_stage(stage_name='BuildAndDeploy', actions=[
             actions.CodeBuildAction(
-                action_name='CodeBuildProject',
+                action_name='BuildAndDeploy',
                 input=source_output,
                 outputs=[build_output],
                 project=build_project,
