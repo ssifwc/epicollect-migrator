240cfa1f31f95657f142dff81663f3b1f630ed15
gioele <gioele@gioele.com>
2021-05-14 16:29:19 -0700

    if there is any issue calling V2, just ignore it. if it's available, it will work/migrate

--- epicollect_importer/handler.py
+++ epicollect_importer/handler.py
@@ -18,12 +18,14 @@ def handle(_, __):
     print('getting points from epicollect ... ')
     database = Database.connect(connection_uri=os.environ['DATABASE_CONNECTION_URI'])
 
-    if os.environ.get('V2') and os.environ['V2'] == 'true':
+    try:
         print('migrating V2 data')
         observations_v2 = epicollect_v2.get_field_observations()
         database.add_field_observations(observations_v2)
         print('Done migrating V2 data')
-        time.sleep(60) # epicollect throttles when too many requests are done to quickly
+        time.sleep(60) # epicollect throttles when too many requests are done too quickly, let's hang out here a bit
+    except:
+        print('Aborting V2 migration, any exception just assumes it is turned off (no public access)')
 
     print('migrating V3 data')
     observations_v3 = epicollect_v3.get_field_observations()
@@ -36,3 +38,5 @@ def handle(_, __):
 
 if __name__ == '__main__':
     handle({}, {})
+
+
